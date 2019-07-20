03f7f9fbbf5d7b54430e5384b4015975f9958e13
tobor <rosborne@osbornepro.com>
2019-07-19 23:07:17 -0600

    Create Find-ExpiringAccounts.ps1

---
+++ Find-ExpiringAccounts.ps1
@@ -0,0 +1,88 @@
+<#
+.Synopsis
+    Find-ExpiringAccounts is a cmdlet created for Task Scheduler to find expiring accounts and alert the appropriate people. 
+    
+.DESCRIPTION
+    This cmdlet finds accounts that are expiring soon and can alert through email this is happening in the next 10 days.
+
+.NOTES
+    Author: Rob Osborne 
+    Alias: tobor
+    Contact: rosborne@osbornepro.com
+    https://roberthosborne.com
+
+.EXAMPLE
+   Find-ExpiringAccounts
+
+.EXAMPLE
+   Find-ExpiringAccounts -Verbose
+#>
+
+Function Find-File {
+    [CmdletBinding()]
+        param() # End param
+
+  BEGIN {
+
+    $SmtpServer = <Smtp_Server>
+    
+    $To = "hremailaddress@osbornepro.com","itemailaddress@osbornepro.com"
+    
+    $FromEmail = "from@osbornepro.com"
+
+    $Accounts = Search-ADAccount -AccountExpiring -TimeSpan "10.00:00:00" | Select-Object -Property AccountExpirationDate, Name, @{ Label = "Manager"; E = { (Get-Aduser(Get-AdUser $_ -Property Manager).Manager).Name } }
+
+  } # End BEGIN
+
+  PROCESS {
+
+    if (!($Accounts -eq $Null)) {
+
+$Css = @"
+<style>
+table {
+    font-family: verdana,arial,sans-serif;
+	font-size:11px;
+	color:#333333;
+	border-width: 1px;
+	border-color: #666666;
+	border-collapse: collapse;
+}
+th {
+	border-width: 1px;
+	padding: 8px;
+	border-style: solid;
+	border-color: #666666;
+	background-color: #dedede;
+}
+td {
+	border-width: 1px;
+	padding: 8px;
+	border-style: solid;
+	border-color: #666666;
+	background-color: #ffffff;
+}
+</style>
+"@
+
+    $PreContent = "<Title>Expiring Users (Next 10 Days)</Title>"
+
+    $NoteLine = "$(Get-Date -format 'MM/dd/yyyy HH:mm:ss')"
+
+    $PostContent = "<br><p><font size='2'><i>$NoteLine</i></font>"
+  
+    $Body = $Accounts | ConvertTo-Html -Head $Css -PostContent $PostContent -PreContent $PreContent | Out-String
+
+  } # End PROCESS
+
+  END { 
+
+    ForEach ($ToEmail in $To) {
+    
+      Send-MailMessage -From $FromEmail -To $ToEmail -Subject "AD Event: Accounts Expiring" -BodyAsHtml -Body $Body -SmtpServer $SmtpServer
+    
+    } # End Foreach loop
+  
+  } # End END
+  
+} # End Function
