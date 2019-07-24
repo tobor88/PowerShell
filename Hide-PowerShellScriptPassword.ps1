be3871b048fe0f26e9b39f3f924f77249e7e1603
tobor <rosborne@osbornepro.com>
2019-07-24 11:26:01 -0600

    Create Hide-PowerShellScriptPassword.ps1

---
+++ Hide-PowerShellScriptPassword.ps1
@@ -0,0 +1,119 @@
+<#
+.Synopsis
+    Hide-PowerShellScriptPassword is a cmdlet that is used to create a couple of keys that can be used to prevent storing a password 
+    in clear text inside of a PowerShell script. It creates and AES encrypted file contents and key to unlock the secret and never
+    display the password in clear text. This should be run as an administrator.
+
+.DESCRIPTION
+    Create multiple files that can be used to prevent the usage of clear text passwords stored in clear text.
+    This should be run as an administrator.
+    Get-RandomHexNumber was taken from https://powershell.org/forums/topic/generating-a-20-character-hex-string-with-powershell/ 
+
+.NOTES
+    Author: Rob Osborne 
+    Alias: tobor
+    Contact: rosborne@osbornepro.com
+    https://roberthosborne.com
+
+.EXAMPLE
+   Hide-PowerShellScriptPassword -ContactName $ContactName -ContactEmail $ContactEmail -GroupName "Group1", "Group2"
+
+.EXAMPLE
+   Hide-PowerShellScriptPassword -ContactName $ContactName -ContactEmail $ContactEmail -GroupName $GroupName -Verbose
+#>
+
+Function Hide-PowerShellScriptPassword {
+    [CmdletBinding()]
+        param(
+        [Parameter(Mandatory=$True,
+                Position=0,
+                ValueFromPipeline=$True,
+                ValueFromPipelineByPropertyName=$True,
+                HelpMessage="New Contacts Name. `n Example: Dixie Normus `n`n If you see this message, you will need to enter the new contacts name.")] # End Parameter
+            [ValidateNotNullorEmpty()]
+        [string[]]$ContactName) # End param
+
+    BEGIN {
+    
+       $KeyFilePath = C:\Users\Public\Documents\Keys
+       
+       $AESPasswordPath = C:\Users\Public\Documents\Password
+    
+        Function Get-RandomHexNumber{
+            param( 
+                [int] $length = 20,
+                [string] $chars = "0123456789ABCDEF") # End param
+                
+        $Bytes = New-object "System.Byte[]" $Length
+        
+        $Rnd = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
+        
+        $Rnd.GetBytes($Bytes)
+        
+        $Result = ""
+        
+        1..$Length | Foreach {
+        
+            $Result += $Chars[ $Bytes[$_] ForEach-Object $Chars.Length ]    
+            
+        } # End Foreach
+        
+        $Result
+        
+        } # End Function Get-RandomHexNumber
+        
+    } # End BEGIN
+        
+    PROCESS {
+    
+        Write-Verbose "Creating a random 32-bit key and storing it to a file. (Maximum Key Size is 32)
+
+        $Var = Get-RandomHexNumber -Length 20
+
+        $KeyFile = New-Item -ItemType File -Name $Var -Path $KeyFilePath
+
+        $Key = New-Object Byte[] 32 
+
+        [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+
+        $Key | Out-File $KeyFile 
+        
+        Write-Verbose "Encryption key file has been created."
+        
+    } # End PROCESS
+
+    END {
+
+        Write-Verbose "Invoking the stored key to create the encrypted password"
+        
+        $Pass = Read-Host -Prompt "Enter the password you want to AES encrypt for a script"
+        
+        $PasswordFile = New-Item -ItemType File -Name "$Var.txt" -Path $AESPasswordPath  
+        
+        $KeyFile = "$KeyFilePath\$Var"
+        
+        $Key = Get-Content $KeyFile
+        
+        $Password = "$Pass" | ConvertTo-SecureString -AsPlainText -Force
+        
+        $Password | ConvertFrom-SecureString -key $Key | Out-File $PasswordFile
+
+        Write-Host "Use the below lines to insert your encrypted credentials into the script"
+        Write-Host " "
+        Write-Host '$User = "account@osbornepro.com"'
+        Write-Host '$PasswordFile = "$AESPasswordPath\$Var.txt"'
+        Write-Host '$KeyFile = "$KeyFilePath\$Var"'
+        Write-Host '$key = Get-Content $KeyFile'
+        Write-Host '$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, (Get-Content $PasswordFile | ConvertTo-SecureString -Key $key)'
+        Write-Host " "
+        Write-Host 'Below is an example of how to insert the password value into a Get-Credential type of cmdlet'
+        Write-Host " "
+        Write-Host 'Invoke-Command -Credential $cred'
+        Write-Host " "
+        Write-Host "Add the below variable to insert the password into a string. Someone skilled can reverse this method. `nUse above method if available."
+        Write-Host '$SecurePass = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($cred.Password)'
+        Read-Host '$PassString = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($SecurePass)'
+
+    } # End END
+
+} # End Function Hide-PowerShellScriptPassword
