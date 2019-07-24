f41b37c00d2223486ab88fd13e271313c6fbdc04
tobor <rosborne@osbornepro.com>
2019-07-24 10:56:00 -0600

    Create Backup-CiscoSwitchConfig.ps1
    Can be used to back up the running configuration of a switch.
    
    # REQUIREMENTS #
    #--------------
    #===================================================================================================
    #
    # For this script to work you will need KiTTY installed. This can be changed by you of course.
    # SSH port 22 will need to be open on the switch
    # A TFTP server will need to be listening and accessible over the network on port 69
    #
    #====================================================================================================
    #
    # This backup scripts has been tested with the following Cisco Switch Models
    # -  SG250-26P 26-Port Gigabit PoE Smart Switch 
    # -  SG250-50P 
    # -  Cisco SG300-52 
    # -  Cisco Catalyst 9200 
    #
    #=====================================================================================================

---
+++ Backup-CiscoSwitchConfig.ps1
@@ -0,0 +1,46 @@
+#--------------
+# REQUIREMENTS #
+#--------------
+#===================================================================================================
+#
+# For this script to work you will need KiTTY installed. This can be changed by you of course.
+# SSH port 22 will need to be open on the switch
+# A TFTP server will need to be listening and accessible over the network on port 69
+#
+#====================================================================================================
+#
+# This backup scripts has been tested with the following Cisco Switch Models
+# -  SG250-26P 26-Port Gigabit PoE Smart Switch 
+# -  SG250-50P 
+# -  Cisco SG300-52 
+# -  Cisco Catalyst 9200 
+#
+#=====================================================================================================
+
+$SwitchIP = "192.168.1.5"
+$SshPassword = "P@ssw0rd!"
+$SshUsername = "cisco"
+$LogFilePath = C:\Users\Public\Documents\Logs
+$PathToKiTTY = C:\Users\Public\Desktop\KiTTY.exe
+
+# I formatted the date with the year first for better organization in the folder.
+$FormatedDate = get-date -format 'yyyyMMdd-HHmm'
+
+$Date = Get-Date -Format M.d.yyyy
+
+$ConfigFile = $SwitchIP + "_" + $formatedDate + ".txt" 
+$LogFile = $SwitchIP + "_" + $formatedDate + ".log" 
+$SshCommand = @"
+$username
+$SshPassword
+copy running-config tftp://10.0.2.251/SwitchIP_$Date.txt
+logout
+exit
+logout
+"@
+    $KiTTYCommands = {echo y | $PathToKiTTY $SwitchIP -ssh -v -l $SshUsername -pw $pass -cmd $sshCommand -log "$LogFilePath\$SwitchIP_$FormatedDate.txt"}
+
+    & $KiTTYCommands
+
+    sleep -s 1
+ 
