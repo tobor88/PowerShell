43cff49b08e7c02216148ccb3211635d01d03936
Rob Osborne <rob.osborne@usav.org>
2020-07-04 01:57:11 -0600

    Create Test-LDAPS.ps1

---
+++ Test-LDAPS.ps1
@@ -0,0 +1,106 @@
+<#
+.NAME
+    Test-LDAPS
+
+
+.SYNOPSIS
+    This cmdlet is used to verify your domain controller is correctly configured to accept LDAP over SSL connections
+
+
+.PARAMETER ComputerName
+    Specifies one computer name or a comma-separated array of computer names. This cmdlet accepts
+    ComputerName objects from the pipeline or variables.
+
+    Type the NetBIOS name, an IP address, or a fully qualified domain name of a remote computer. To specify the
+    local computer, type the computer name, a dot `.`, or localhost.
+
+    This parameter doesn't rely on PowerShell remoting. You can use the ComputerName parameter even if your
+    computer isn't configured to run remote commands.
+
+
+.NOTES
+    Author: Robert H. Osborne
+    Alias: tobor
+    Contact: rosborne@osbornepro.com
+
+
+.LINK
+    https://github.com/tobor88
+    https://www.powershellgallery.com/profiles/tobor
+    https://roberthosborne.com
+
+
+.INPUTS
+    System.String
+        Accepts computer names from the pipeline or variables. (Domain Controllers are of course required)
+
+
+.OUTPUTS
+    PSCustomObject
+
+#>
+Function Test-LDAPS {
+    [CmdletBinding()]
+        param (
+            [Parameter(
+                Mandatory=$True,
+                ValueFromPipeLine=$True,
+                ValueFromPipeLineByPropertyName=$True,
+                HelpMessage='Enter the hostname or ip address of a domain controller to test LDAPS on. Separate multiple values with a comma')]
+            [Alias('cn','Computer','Server')]
+	        [String[]]$ComputerName
+        )  # End param
+
+BEGIN
+{
+
+    $Obj = @()
+
+}  # End BEGIN
+PROCESS
+{
+
+    ForEach ($Computadora in $ComputerName)
+    {
+
+        Try
+        {
+
+            Write-Verbose "[*] Attempting to connect to port 636 on $Computadora"
+            $LDAPS = [ADSI]("LDAP://" + $ComputerName + ":636")
+
+        }  # End Try
+        Catch
+        {
+
+            Write-Verbose "[x] Trouble connecting to $Computadora on port 636"
+            $Error[0]
+
+        }  # End Catch
+
+        If ($LDAPS.Path)
+        {
+
+            $Protocol = 'LDAPS'
+
+        }  # End If
+        Else
+        {
+
+            $Protocol = 'x'
+
+        }  # End Else
+
+        $Obj += New-Object -TypeName PSObject -Property @{Server="$Computadora";Protocol="$Protocol"}
+
+    }  # End ForEach
+
+}  # End PROCESS
+END
+{
+
+    $Obj
+
+}  # End END
+
+} # End Test-LDAPS
