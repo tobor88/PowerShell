834e8e774f1018b92a6a31bb8bc54f554e0145a0
Rob Osborne <rob.osborne@usav.org>
2020-09-11 10:37:34 -0600

    Initial commit of remove da profile

---
+++ Remove-DirectAccessProfile.ps1
@@ -0,0 +1,19 @@
+Function Remove-DirectAccessProfile {
+    [CmdletBinding()]
+        param(
+            [Parameter(
+                Position=0,
+                Mandatory=$False,
+                ValueFromPipeline=$True,
+                ValueFromPipelineByPropertyName=$True)]  # End Parameter
+            [Alias('cn')]
+            [String[]]$ComputerName
+        )  # End param
+
+    $RegPath = ‘HKLM:\Software\Policies\Microsoft\Windows NT\DNSClient\DnsPolicyConfig’
+
+
+    Remove-Item -Path "$RegPath\*" -Recurse
+    
+
+}  # End Function Remove-DirectAccessProfile
