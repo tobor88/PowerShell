e19a7122c6a2edad5a708c9a947b85b5efcaf43a
tobor <rosborne@osbornepro.com>
2022-02-28 14:01:38 -0700

    Update Set-NewLDAPSCertificate.ps1
    Modified to update expiring LDAP certificates

--- Set-NewLDAPSCertificate.ps1
+++ Set-NewLDAPSCertificate.ps1
@@ -13,12 +13,35 @@ $ServiceNames = "NTDS",((Get-CimInstance -ClassName Win32_Service -Filter 'Name 
 
 Write-Output "[*] Obtaining LDAP over SSL certificate by Template Name from the local machine certificate store"
 $LDAPSCert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object -FilterScript { $_.Extensions | Where-Object -FilterScript { ($_.Oid.FriendlyName -eq "Certificate Template Information") -and ($_.Format(0) -Match $LDAPSTemplateName) }}
+$ExpiringCert = Get-ChildItem -Path Cert:\LocalMachine\My -ExpiringInDays 30 | Where-Object -FilterScript { $_.Extensions | Where-Object -FilterScript { ($_.Oid.FriendlyName -eq "Certificate Template Information") -and ($_.Format(0) -Match $LDAPSTemplateName) }}
+$Path = "HKLM:\SOFTWARE\Microsoft\SystemCertificates\MY\Certificates\$($LDAPSCert.Thumbprint)"
+
+If (($LDAPSCert -Contains $ExpiringCert) -and ($Null -ne $LDAPSCert[1])) {
+
+    $LDAPSCert = $LDAPSCert | Where-Object -Property Thumbprint -ne $ExpiringCert.Thumbprint
+
+}  # End If
+ElseIf ($LDAPSCert -eq $ExpiringCert) {
+
+    Write-Output "[*] Renewing LDAPS certificate with the same keys"
+    Start-Process -WorkingDirectory "C:\Windows\System32" -FilePath certreq.exe -ArgumentList @('-Enroll', '-machine', '-q', '-cert', $LDAPSCert.SerialNumber, 'Renew', 'ReuseKeys')
+
+    $LDAPSCert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object -FilterScript { $_.Extensions | Where-Object -FilterScript { ($_.Oid.FriendlyName -eq "Certificate Template Information") -and ($_.Format(0) -Match $LDAPSTemplateName) }}
+    $ExpiringCert = Get-ChildItem -Path Cert:\LocalMachine\My -ExpiringInDays 30 | Where-Object -FilterScript { $_.Extensions | Where-Object -FilterScript { ($_.Oid.FriendlyName -eq "Certificate Template Information") -and ($_.Format(0) -Match $LDAPSTemplateName) }}
+    If ($LDAPSCert -ne $ExpiringCert) {
+
+        Write-Output "[*] Deleting the old certificate from the LocalMachine Certificate store. New certificate has a different thumbprint"
+        Get-ChildItem -Path "Cert:\LocalMachine\My\$($ExpiringCert.Thubprint)" | Remove-Item -Force
+
+    }  # End If
+
+}  # End ElseIf
 
 # The commented area below is just in case we need to export or import a PFX certificate into the localmachine store
 #
 #Write-Output "[*] Exporting LDAPS certificate from LocalMachine store"
 #Export-PfxCertificate -FilePath $CertPath -Password $SecurePassword
-#
+
 #Write-Output "[*] Importing new PFX LDAPS Certificate into store"
 #Import-PfxCertificate -FilePath $CertPath -CertStoreLocation "Cert:\LocalMachine\My" -Confirm:$False -Password $SecurePassword -Exportable
 
@@ -28,13 +51,13 @@ ForEach ($ServiceName in $ServiceNames) {
 
     If ($ServiceName.Length -gt 0) {
 
-        If (Test-Path -Path "HKLM:\SOFTWARE\Microsoft\SystemCertificates\MY\Certificates\$($LDAPSCert.Thumbprint)") {
+        If (Test-Path -Path $Path) {
         
-            Write-Output "[*] Moving PFX certificate into the NTDS\Personal Certificate Store "
-            Move-Item -Path "HKLM:\SOFTWARE\Microsoft\SystemCertificates\MY\Certificates\$($LDAPSCert.Thumbprint)" -Destination "HKLM:\SOFTWARE\Microsoft\Cryptography\Services\$ServiceName\SystemCertificates\MY\Certificates\"
+            Write-Output "[*] Moving PFX certificate into the NTDS\Personal Certificate Store"
+            Move-Item -Path $Path -Destination "HKLM:\SOFTWARE\Microsoft\Cryptography\Services\$ServiceName\SystemCertificates\MY\Certificates\"
 
             Write-Output "[*] Restarting the $ServiceName service"
-            Restart-Service -Name $ServiceName -Force -ErrorAction Inquire
+            Restart-Service -Name $ServiceName -Force
 
         }  # End If
         Else {
